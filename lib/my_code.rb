025440623e4ff17a2d672b14045967bc8c9c081a
Bahay Gulle Bilgi <bahayg@gmail.com>
2019-10-06 15:52:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,10 @@
 # Your Code Here
-def my_own_map (source_array) {|n| n*-1}
+def map (source_array)
+  new = []
+  i = 0 
+  while i < source_array.length do
+    new.push(yield(source_array[i]))
+    i += 1 
+  end
+  yield
 end
