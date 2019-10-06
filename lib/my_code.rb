e8de99e27eb8dc9287c44ac87be9f6c9b831a215
Bahay Gulle Bilgi <bahayg@gmail.com>
2019-10-06 16:06:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,14 +9,14 @@ def map(source_array)
   new
 end
 
-def reduce(source_array, starting_point=0)
+def reduce(source_array, starting_point=nil)
   if starting_point
     total = starting_point
     i = 0
   else 
     total = source_array[0]
     i = 1
-  while i < source_array.length 
+  while i < source_array.length do
     total = yield(total, source_array[i])
     i += 1
   end 
