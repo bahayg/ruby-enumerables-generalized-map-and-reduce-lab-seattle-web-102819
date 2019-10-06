335fba6fdf9ac5b46f0020b7d6ec7bda5a09c94b
Bahay Gulle Bilgi <bahayg@gmail.com>
2019-10-06 16:06:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,6 +16,7 @@ def reduce(source_array, starting_point=nil)
   else 
     total = source_array[0]
     i = 1
+  end
   while i < source_array.length do
     total = yield(total, source_array[i])
     i += 1
